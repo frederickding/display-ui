46fa66824c943df380dadfe77288b55aee4575fe
frederickjding@gmail.com <frederickjding@gmail.com@fc8382f0-57c5-11de-b31f-81d54249e7d9>
2009-06-14 03:08:41 +0000

    Now we've got weather forecast data

--- trunk/server/application/modules/api/controllers/WeatherController.php
+++ trunk/server/application/modules/api/controllers/WeatherController.php
@@ -34,4 +34,38 @@ class Api_WeatherController extends Zend_Controller_Action
 			$this->render();
 		}
 	}
+	public function forecastAction ()
+	{
+		// don't let Zend Framework render a view
+		$this->_helper->viewRenderer->setNoRender();
+		$YWeather = new Api_Model_YWeather(
+			$this->getRequest()->getParam('location'));
+
+		// authenticate the request
+		$Authenticator = new Api_Model_Authenticator();
+		$sys_name = $this->getRequest()->getParam('sys_name');
+		$api_key = $this->getRequest()->getParam('api_key');
+		if ($Authenticator->verify($sys_name, $api_key)) {
+			$this->getResponse()->setHeader('Content-Type', 'text/plain', true);
+			switch($this->getRequest()->getParam('day')) {
+				case 1:
+					echo $YWeather->forecast(1,'high')."\n"
+						.$YWeather->forecast(1,'low')."\n"
+						.$YWeather->forecast(1,'description')."\n"
+						.$YWeather->forecast(1,'code');
+					break;
+				case 0:
+					echo $YWeather->forecast(0,'high')."\n"
+						.$YWeather->forecast(0,'low')."\n"
+						.$YWeather->forecast(0,'description')."\n"
+						.$YWeather->forecast(0,'code');
+			}
+		} else {
+			$this->getResponse()->setHttpResponseCode(401);
+			$this->view->message = 'Unauthenticated Request';
+			$this->view->explanation = 'All API calls to this service must include a '
+				.'valid system identifier and API key.';
+			$this->render('index');
+		}
+	}
 }
--- trunk/server/application/modules/api/models/YWeather.php
+++ trunk/server/application/modules/api/models/YWeather.php
@@ -2,13 +2,18 @@
 /**
  * Parses data from the Yahoo! Weather service
  *
+ * @author Frederick
+ * @version $Id$
  */
 class Api_Model_YWeather
 {
 	private $_location;
 	private $_conditions;
+	private $_forecast;
 	public function __construct ($location)
 	{
+		// No default location is used
+		// CAXX0401 is Richmond Hill
 		$this->_location = $location;
 		Zend_Feed::registerNamespace('yweather', 'http://xml.weather.yahoo.com/ns/rss/1.0');
 	}
@@ -16,19 +21,10 @@ class Api_Model_YWeather
 	{
 		$channel = new Zend_Feed_Rss('http://weather.yahooapis.com/forecastrss?u=c&p=' . $this->_location);
 		return $channel;
-		/* $remote_feed = new Zend_Http_Client(
-			'http://weather.yahooapis.com/forecastrss',
-			array('maxredirects' => 0 , 'timeout' => 15)
-			);
-		$remote_feed->setParameterGet(array(
-			'u' => 'c' ,
-			'p' => $location
-			));
-		return $remote_feed->request()->getBody(); */
 	}
 	public function conditions ($r)
 	{
-		if(!isset($this->_conditions['code']))
+		if(!isset($this->_conditions[$r]))
 		{
 			$_data = $this->retrieve()->current()->{'yweather:condition'}->getDOM();
 			$this->_conditions = array(
@@ -37,5 +33,29 @@ class Api_Model_YWeather
 				'code' => $_data->getAttribute('code'));
 		}
 		return $this->_conditions[$r];
+	}
+	public function forecast ($d=0,$param)
+	{
+		if(!isset($this->_forecast[0]))
+		{
+			$_data0 = $this->retrieve()->current()->{'yweather:forecast'}[0]->getDOM();
+			$_data1 = $this->retrieve()->current()->{'yweather:forecast'}[1]->getDOM();
+			$this->_forecast = array(
+				0 => array(
+					'day' => $_data0->getAttribute('day'),
+					'date' => $_data0->getAttribute('date'),
+					'high' => $_data0->getAttribute('high'),
+					'low' => $_data0->getAttribute('low'),
+					'description' => $_data0->getAttribute('text'),
+					'code' => $_data0->getAttribute('code')),
+				1 => array(
+					'day' => $_data1->getAttribute('day'),
+					'date' => $_data1->getAttribute('date'),
+					'high' => $_data1->getAttribute('high'),
+					'low' => $_data1->getAttribute('low'),
+					'description' => $_data1->getAttribute('text'),
+					'code' => $_data1->getAttribute('code')));
+		}
+		return $this->_forecast[$d][$param];
 	}
 }
