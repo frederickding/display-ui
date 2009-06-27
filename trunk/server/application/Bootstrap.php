9f6802bd8b415f493e661da2eb20cab8dc216b00
Frederick Ding <Frederick Ding@fc8382f0-57c5-11de-b31f-81d54249e7d9>
2009-06-27 00:42:17 +0000

    API is now completely modularized

--- trunk/server/application/Bootstrap.php
+++ trunk/server/application/Bootstrap.php
@@ -1,13 +1,3 @@
 <?php
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
-{
-	protected function _initAutoload ()
-	{
-		$autoloader = new Zend_Application_Module_Autoloader(array(
-				'namespace' => 'Api_' ,
-				'basePath' => dirname(__FILE__) . '/modules/api'
-				));
-		return $autoloader;
-	}
-}
-
+{}
