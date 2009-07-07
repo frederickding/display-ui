ed1e39e253d4b18e626047c106db28732aad14fa
Frederick Ding <Frederick Ding@fc8382f0-57c5-11de-b31f-81d54249e7d9>
2009-07-07 00:37:17 +0000

    More complete login logic

--- trunk/server/application/modules/admin/controllers/IndexController.php
+++ trunk/server/application/modules/admin/controllers/IndexController.php
@@ -26,8 +26,19 @@
  */
 class Admin_IndexController extends Zend_Controller_Action
 {
+	public function init()
+	{
+		$this->base_uri = explode('/admin', $_SERVER['REQUEST_URI']);
+		$this->base_uri = $this->base_uri[0];
+		// initiate a session for the installer
+		$this->auth_session = new Zend_Session_Namespace('auth');
+	}
 	public function indexAction()
 	{
 		// TODO implement actions
+		$this->_helper->viewRenderer->setNoRender();
+		if(!$this->auth_session->authenticated) {
+			$this->_redirect('http://'.$_SERVER['SERVER_NAME'].$this->base_uri.'/admin/login/');
+		}
 	}
 }
