6ebd307f2e941c877b42360642437aa7f80fad95
Frederick <Frederick@fc8382f0-57c5-11de-b31f-81d54249e7d9>
2009-08-20 01:52:38 +0000

    Made formatting more consistent, removed unnecessary member $_config which was replaced with $_secret.

--- trunk/server/application/modules/api/models/Authenticator.php
+++ trunk/server/application/modules/api/models/Authenticator.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /**
  * Authenticator model
  *
@@ -21,61 +21,63 @@
  * @license http://code.google.com/p/display-ui/wiki/License Apache License 2.0
  * @version $Id$
  */
- /**
+/**
  * Authenticates API requests
  */
-class Api_Model_Authenticator {
-    /**
-     * Stores today's date in YYYY-MM-DD format, GMT
-     *
-     * @var string
-     */
-    private $_date = '';
-    /**
-     * Holds the configuration read from Zend_Config
-     *
-     * @var Zend_Config_Ini
-     */
-    private $_config;
-    /**
-     * Constructor method
-     *
-     * Loads the configuration, gets the current GMT date
-     */
-    public function __construct() {
-        $this->_config = new Zend_Config_Ini(CONFIG_DIR.'/configuration.ini', 'production');
-        $this->_date = gmdate('Y-m-d');
-    }
-    /**
-     * Authentication parameters verification method
-     *
-     * Checks whether provided system name and signature are valid
-     *
-     * @param string|int $sys_name the name of the client system
-     * @param string $signature the signature of the request
-     * @return boolean
-     */
-    public function verify($sys_name = '', $signature = '') {
-        // Make sure required parameters are provided
-        if (!$sys_name || !$signature || strlen($signature) != 40)
-            return false;
-        // Check whether a valid API key matches the provided key
-        elseif (sha1($this->apiKey($sys_name).$this->_date) == $signature)
-            return true;
-        else
-            return false;
-    }
+class Api_Model_Authenticator
+{
+	/**
+	 * Stores today's date in YYYY-MM-DD format, GMT
+	 *
+	 * @var string
+	 */
+	private $_date = '';
+	/**
+	 * Holds the secret read from Zend_Config
+	 *
+	 * @var string
+	 */
+	private $_secret;
+	/**
+	 * Constructor method
+	 *
+	 * Loads the configuration, gets the current GMT date
+	 */
+	public function __construct ()
+	{
+		$config = new Zend_Config_Ini(CONFIG_DIR . '/configuration.ini', 'production');
+		$this->_secret = $config->production->server->install->secret;
+		unset($config);
+		$this->_date = gmdate('Y-m-d');
+	}
+	/**
+	 * Authentication parameters verification method
+	 *
+	 * Checks whether provided system name and signature are valid
+	 *
+	 * @param string|int $sys_name the name of the client system
+	 * @param string $signature the signature of the request
+	 * @return boolean
+	 */
+	public function verify ($sys_name = '', $signature = '')
+	{
+		// Make sure required parameters are provided
+		if (! $sys_name || ! $signature || strlen($signature) != 40) return false;
+		// Check whether a valid API key matches the provided key
+		elseif (sha1($this->apiKey($sys_name) . $this->_date) == $signature) return true;
+		else return false;
+	}
 	/**
 	 * API key generator method
-	 * 
+	 *
 	 * Generates a valid internal API key for the given client system name
 	 * @param string $sys_name the name of the client system
 	 * @return false|string
 	 */
-	public function apiKey($sys_name = '') {
+	public function apiKey ($sys_name = '')
+	{
 		// Make sure required parameter is provided
-		if (!$sys_name)
-			return false;
-		return hash_hmac('sha256', $sys_name, $this->_config->production->server->install->secret);
+		if (! $sys_name) return false;
+		return hash_hmac('sha256', $sys_name, $this->_secret);
 	}
 }
