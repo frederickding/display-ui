ed3c21b0e9d47f1ea6a3c69969d47bae61f9d4e6
Frederick <Frederick@fc8382f0-57c5-11de-b31f-81d54249e7d9>
2009-09-05 03:20:42 +0000

    Make use of baseUrl view helper and url() function to generate consistent relative links

--- trunk/server/application/controllers/IndexController.php
+++ trunk/server/application/controllers/IndexController.php
@@ -1,22 +1,6 @@
 <?php
-
 class IndexController extends Zend_Controller_Action
 {
-
-    public function init()
-    {
-		if(strpos($_SERVER['REQUEST_URI'], 'index')===FALSE) {
-			$this->base_uri = rtrim($_SERVER['REQUEST_URI'],'/');
-		} else {
-			$this->base_uri = explode('/index', $_SERVER['REQUEST_URI']);
-			$this->base_uri = $this->base_uri[0];
-		}
-    }
-
-    public function indexAction()
-    {
-        // action body
-		$this->view->base_uri = $this->base_uri;
-		
-    }
+	public function indexAction ()
+	{}
 }
