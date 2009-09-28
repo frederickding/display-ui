fbac592508b8822151b7df19f3b61e673ae3a9b8
frederickjding <frederickjding@fc8382f0-57c5-11de-b31f-81d54249e7d9>
2009-09-28 02:18:31 +0000

    Additional features added. See http://code.google.com/p/display-ui/wiki/NewInServer#Version_0.2.4alpha for a summary of improvements.

---
+++ tags/server-0.2.4alpha/application/controllers/structure.mysql.sql
@@ -0,0 +1,101 @@
+-- -----------------------------------------------------------------------------
+-- SQL database structure for Display UI Server
+-- -----------------------------------------------------------------------------
+
+--
+-- Table structure for table `dui_users`
+--
+
+DROP TABLE IF EXISTS `dui_users`;
+CREATE TABLE IF NOT EXISTS `dui_users` (
+  `id`			int(10) unsigned NOT NULL auto_increment,
+  `username`	varchar(64) NOT NULL,
+  `password`	varchar(64) NOT NULL,
+  `email`		varchar(128) NOT NULL,
+  `last_active`	datetime NOT NULL,
+  `acl_role`	varchar(64) NOT NULL,
+  PRIMARY KEY  (`id`,`username`),
+  KEY `email` (`email`)
+) DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- Table structure for table `dui_clients`
+--
+
+DROP TABLE IF EXISTS `dui_clients`;
+CREATE TABLE IF NOT EXISTS `dui_clients` (
+  `id`			int(10) unsigned NOT NULL auto_increment,
+  `sys_name`	varchar(64) NOT NULL,
+  `last_active`	datetime NOT NULL,
+  `admin`		int(10) unsigned NOT NULL,
+  `users`		varchar(64) NOT NULL,
+  `location`	varchar(16) NOT NULL,
+  PRIMARY KEY  (`id`)
+) DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- Table structure for table `dui_headlines`
+--
+
+DROP TABLE IF EXISTS `dui_headlines`;
+CREATE TABLE IF NOT EXISTS `dui_headlines` (
+  `id`			int(10) unsigned NOT NULL auto_increment,
+  `title`		varchar(256) NOT NULL,
+  `active`		tinyint(1) NOT NULL,
+  `expires`		datetime NOT NULL,
+  `type`		varchar(32) NOT NULL,
+  `client`		int(10) unsigned NOT NULL,
+  PRIMARY KEY  (`id`),
+  KEY `title` (`title`),
+  KEY `type` (`type`)
+) DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- Table structure for table `dui_options`
+--
+
+DROP TABLE IF EXISTS `dui_options`;
+CREATE TABLE IF NOT EXISTS `dui_options` (
+  `id`			int(10) unsigned NOT NULL auto_increment,
+  `option_name`	varchar(64) NOT NULL,
+  `option_value` mediumtext NOT NULL,
+  `client_id`	int(10) unsigned NOT NULL DEFAULT 0,
+  PRIMARY KEY  (`id`),
+  KEY `option_name` (`option_name`)
+) DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- Table structure for table `dui_media`
+--
+
+DROP TABLE IF EXISTS `dui_media`;
+CREATE TABLE IF NOT EXISTS `dui_media` (
+  `id`			int(10) unsigned NOT NULL auto_increment,
+  `title`		varchar(64) NOT NULL,
+  `activates`	datetime NOT NULL,
+  `expires`		datetime NULL,
+  `active`		tinyint(1) NOT NULL,
+  `type`		varchar(32) NOT NULL,
+  `clients`		varchar(64) NULL,
+  `weight`		smallint NOT NULL DEFAULT 1,
+  `content`		varchar(128) NOT NULL,
+  `data`		mediumblob NULL,
+  PRIMARY KEY (`id`),
+  KEY `title` (`title`)
+) DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- Table structure for table `dui_playlists`
+--
+
+DROP TABLE IF EXISTS `dui_playlists`;
+CREATE TABLE IF NOT EXISTS `dui_playlists` (
+  `id`			int(10) unsigned NOT NULL auto_increment,
+  `generated`	datetime NOT NULL,
+  `revision`	tinyint NOT NULL DEFAULT 1,
+  `client`		int(10) unsigned NOT NULL,
+  `played`		datetime NULL,
+  `content`		mediumtext NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `client` (`client`)
+) DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
---
+++ tags/server-0.2.4alpha/application/modules/admin/controllers/HeadlinesController.php
@@ -0,0 +1,173 @@
+<?php
+/**
+ * Headlines controller for Admin module
+ *
+ * Copyright 2009 Frederick Ding<br />
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ * 		http://www.apache.org/licenses/LICENSE-2.0
+ * or the full licensing terms for this project at
+ * 		http://code.google.com/p/display-ui/wiki/License
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author Frederick
+ * @license http://code.google.com/p/display-ui/wiki/License Apache License 2.0
+ * @version $Id$
+ */
+/**
+ * Functionality for managing headlines
+ */
+class Admin_HeadlinesController extends Zend_Controller_Action
+{
+	public function init ()
+	{
+		// initiate a session for the installer
+		$this->auth_session = new Zend_Session_Namespace('auth');
+		// ALWAYS check if authenticated
+		if (! $this->auth_session->authenticated) {
+			return $this->_redirect($this->view->serverUrl() . $this->view->url(array(
+				'module' => 'admin' ,
+				'controller' => 'login' ,
+				'action' => 'index')));
+		}
+		// configuration object
+		if (Zend_Registry::isRegistered('configuration_ini')) {
+			$config = Zend_Registry::get('configuration_ini');
+		} else {
+			$config = new Zend_Config_ini(CONFIG_DIR . '/configuration.ini',
+				'production');
+			Zend_Registry::set('configuration_ini', $config);
+		}
+		$this->view->systemName = $config->server->install->name;
+		$this->view->username = $this->auth_session->username;
+		$this->_helper->layout()->setLayout('AdminPanelWidgets');
+		return TRUE;
+	}
+	public function indexAction ()
+	{
+		$this->_forward('list');
+	}
+	public function listAction ()
+	{
+		$HeadlinesModel = new Admin_Model_Headlines();
+		$this->view->headlinesList = $HeadlinesModel->fetchHeadlines($this->auth_session->username);
+		$this->view->insertForm = $this->insertForm();
+	}
+	/**
+	 * Shows a confirmation page for deletion of an item
+	 */
+	public function deleteAction ()
+	{
+		$this->view->id = (int) $this->_getParam('id');
+		$HeadlinesModel = new Admin_Model_Headlines();
+		$this->auth_session->deleteCrsf = sha1(microtime(TRUE));
+		$this->view->csrf = $this->auth_session->deleteCrsf;
+		$this->view->canDelete = $HeadlinesModel->canModify($this->auth_session->username, $this->view->id);
+	}
+	public function deleteProcessAction ()
+	{
+		$this->_helper->viewRenderer->setNoRender();
+		$this->_helper->removeHelper('layout');
+		if (/*$this->getRequest()->isPost() &&*/ $this->_getParam('deletecrsf') == $this->auth_session->deleteCsrf) {
+			$HeadlinesModel = new Admin_Model_Headlines();
+			$id = (int) $this->_getParam('id', 0);
+			if ($this->_getParam('deleteconf', 'No!') == 'Yes!') {
+				if ($HeadlinesModel->deleteHeadline($id)) {
+					return $this->getResponse()->setBody('Successfully deleted.');
+				} else {
+					return $this->getResponse()->setBody('Deletion failed.');
+				}
+			}
+		}
+		return $this->getResponse()->setBody('Nothing was deleted. <a href="javascript:history.back(1);">Go back.</a>');
+	}
+	public function insertProcessAction ()
+	{
+		if (! $this->getRequest()->isPost()) {
+			return $this->_redirect($this->view->serverUrl() . $this->view->url(array(
+				'module' => 'admin' ,
+				'controller' => 'headlines' ,
+				'action' => 'list')));
+		}
+		$form = $this->insertForm();
+		$HeadlinesModel = new Admin_Model_Headlines();
+		if ($form->isValid($_POST)) {
+			$values = $form->getValues();
+			$this->view->success = ($HeadlinesModel->insertHeadline($values['headlinemessage'], $values['headlineclient'], $values['headlinetype'], $values['headlineexpires']));
+			return $this->render('insert-process');
+		} else {
+			$this->view->insertForm = $form;
+			$this->view->headlinesList = $HeadlinesModel->fetchHeadlines($this->auth_session->username);
+			return $this->render('list');
+		}
+	}
+	public function insertForm ()
+	{
+		$DashboardModel = new Admin_Model_Dashboard();
+		$listClients = $DashboardModel->fetchClients($this->auth_session->username);
+		unset($DashboardModel);
+		$this->view->doctype('XHTML1_STRICT');
+		$message = new Zend_Form_Element_Textarea('headlinemessage',
+			array(
+				'label' => 'Add a headline message' ,
+				'required' => TRUE));
+		$message->setAttribs(array(
+			'cols' => 50 ,
+			'rows' => 3));
+		$show = new Zend_Form_Element_Select('headlineclient',
+			array(
+				'label' => 'Show on' ,
+				'required' => TRUE));
+		foreach ($listClients as $c) {
+			$show->addMultiOption($c['id'], $c['sys_name']);
+		}
+		$type = new Zend_Form_Element_Select('headlinetype',
+			array(
+				'label' => 'Message type' ,
+				'required' => TRUE ,
+				'description' => 'Most headlines are news. PSAs are low-priority messages such as "Weather data provided by Yahoo! Weather."'));
+		$type->addMultiOptions(array(
+			'news' => 'News' ,
+			'psa' => 'Public Service Announcement'));
+		$expires = new Zend_Form_Element_Text('headlineexpires',
+			array(
+				'label' => 'Stop showing on' ,
+				'required' => FALSE ,
+				// TODO: better local timezone support using CONVERT_TZ() in SQL
+				'description' => 'Enter in YYYY-MM-DD format. If you use the more specific YYYY-MM-DD HH:MM:SS format, it must be in UTC. If this is blank, the headline will, by default, expire in 1 month.'));
+		$expires->addFilter('Digits');
+		$submit = new Zend_Form_Element_Submit('headlinesubmit',
+			array(
+				'label' => 'Save'));
+		$submit->setDecorators(array(
+			'ViewHelper'));
+		$reset = new Zend_Form_Element_Reset('headlinereset',
+			array(
+				'label' => 'Reset'));
+		$reset->setDecorators(array(
+			'ViewHelper'));
+		$csrf = new Zend_Form_Element_Hash('headlinecsrf', array('salt' => 'unique'));
+		$csrf->removeDecorator('HtmlTag')->removeDecorator('Label');
+		$form = new Zend_Form();
+		$form->setAction($this->view->url(array(
+			'module' => 'admin' ,
+			'controller' => 'headlines' ,
+			'action' => 'insert-process')))->setMethod('post')->setAttrib('id', 'headlineinsert')->addElements(array(
+			'headlinemessage' => $message ,
+			'headlineclient' => $show ,
+			'headlinetype' => $type ,
+			'headlineexpires' => $expires ,
+			'headlinecsrf' => $csrf ,
+			'headlinesubmit' => $submit ,
+			'headlinereset' => $reset));
+		// $form->removeDecorator('HtmlTag');
+		return $form;
+	}
+}
