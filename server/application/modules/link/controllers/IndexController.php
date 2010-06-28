6170ce44d1554f445b92730e7ffa0f18427d051c
Frederick <Frederick@fc8382f0-57c5-11de-b31f-81d54249e7d9>
2010-06-28 00:57:18 +0000

    Enhanced link module to be more useful to admins.

--- server/application/modules/link/controllers/IndexController.php
+++ server/application/modules/link/controllers/IndexController.php
@@ -1,11 +1,26 @@
 <?php
 /**
- * IndexController
+ * Link module index controller
+ *
+ * Copyright 2010 Frederick Ding<br />
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ * 		http://www.apache.org/licenses/LICENSE-2.0
+ * or the full licensing terms for this project at
+ * 		http://code.google.com/p/display-ui/wiki/License
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  * @author Frederick
+ * @license http://code.google.com/p/display-ui/wiki/License Apache License 2.0
  * @version $Id$
  */
-// require_once 'Zend/Controller/Action.php';
 class Link_IndexController extends Zend_Controller_Action
 {
 	public function init ()
@@ -22,14 +37,7 @@ class Link_IndexController extends Zend_Controller_Action
 				'controller' => $this->_request->getControllerName() ,
 				'action' => $this->_request->getActionName())));
 		}
-		// configuration object
-		if (Zend_Registry::isRegistered('configuration_ini')) {
-			$config = Zend_Registry::get('configuration_ini');
-		} else {
-			$config = new Zend_Config_ini(CONFIG_DIR . '/configuration.ini',
-				'production');
-			Zend_Registry::set('configuration_ini', $config);
-		}
+
 		// ACL
 		if (! Zend_Registry::isRegistered('dui_acl')) {
 			$this->acl = new Admin_Model_Acl();
@@ -42,6 +50,17 @@ class Link_IndexController extends Zend_Controller_Action
 		if (! $this->acl->isAllowed($this->user_role, 'clients', 'link') && $this->_request->getActionName() != 'unauthorized') {
 			$this->_forward('unauthorized');
 		}
+
+		// configuration object
+		if (Zend_Registry::isRegistered('configuration_ini')) {
+			$config = Zend_Registry::get('configuration_ini');
+		} else {
+			$config = new Zend_Config_ini(CONFIG_DIR . '/configuration.ini',
+				'production');
+			Zend_Registry::set('configuration_ini', $config);
+		}
+
+		$this->clients_model = new Admin_Model_Clients();
 	}
 	/**
 	 * The default action
@@ -50,10 +69,9 @@ class Link_IndexController extends Zend_Controller_Action
 	{
 		$this->_helper->layout()->setLayout('SimpleBlue');
 		$client_auth = new Api_Model_Authenticator();
-		$clients_model = new Admin_Model_Clients();
-		$list_clients = $clients_model->fetchClients($this->auth_session->username);
+		$list_clients = $this->clients_model->fetchClients($this->auth_session->username);
 		$this->view->api_keys = array();
-		for($i = 0; $i < count($list_clients); $i++) {
+		for ($i = 0; $i < count($list_clients); $i ++) {
 			$this->view->api_keys[$i][0] = $list_clients[$i]['sys_name'];
 			$this->view->api_keys[$i][1] = $client_auth->apiKey($list_clients[$i]['sys_name']);
 		}
