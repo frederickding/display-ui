c411257c3efaaf2076acbd991a555d69fcc39162
frederickjding@gmail.com <frederickjding@gmail.com@fc8382f0-57c5-11de-b31f-81d54249e7d9>
2010-07-08 01:36:44 +0000

    Retagging 0.3.1-alpha for server component

---
+++ tags/server-0.3.1-alpha/modules/admin/controllers/ClientsController.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * ClientsController for Admin module
+ *
+ * Copyright 2009 Frederick Ding<br />
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ * 		http://www.apache.org/licenses/LICENSE-2.0
+ * or the full licensing terms for this project at
+ * 		http://code.google.com/p/display-ui/wiki/License
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author Frederick
+ * @license http://code.google.com/p/display-ui/wiki/License Apache License 2.0
+ * @version $Id$
+ */
+/**
+ * Functionality for managing clients
+ */
+class Admin_ClientsController extends Zend_Controller_Action
+{
+	public function init ()
+	{
+		// initiate a session for the installer
+		$this->auth_session = new Zend_Session_Namespace('auth');
+		// ALWAYS check if authenticated
+		if (! $this->auth_session->authenticated) {
+			return $this->_redirect($this->view->serverUrl() . $this->view->url(array(
+				'module' => 'admin' ,
+				'controller' => 'login' ,
+				'action' => 'index')) . '?redirect=' . $this->view->url(array(
+				'module' => 'admin' ,
+				'controller' => $this->_request->getControllerName() ,
+				'action' => $this->_request->getActionName())));
+		}
+			// configuration object
+		if(Zend_Registry::isRegistered('configuration_ini')) {
+			$config = Zend_Registry::get('configuration_ini');
+		} else {
+			$config = new Zend_Config_ini(CONFIG_DIR . '/configuration.ini', 'production');
+			Zend_Registry::set('configuration_ini', $config);
+		}
+		$this->view->systemName = $config->server->install->name;
+		$this->view->username = $this->auth_session->username;
+		$this->_helper->layout()->setLayout('AdminPanelWidgets');
+		return TRUE;
+	}
+	public function indexAction ()
+	{
+		$this->_forward('list');
+	}
+	public function listAction ()
+	{
+		$ClientsModel = new Admin_Model_Clients();
+		$this->view->clientsList = $ClientsModel->fetchClients($this->auth_session->username);
+	}
+}
