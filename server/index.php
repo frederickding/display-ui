728ac22071c00b48ecac8e70e791be5da853bc2f
Frederick <Frederick@fc8382f0-57c5-11de-b31f-81d54249e7d9>
2011-06-24 05:49:21 +0000

    Updated version tag in files to 0.5.1-beta in preparation for tag.

--- server/index.php
+++ server/index.php
@@ -2,7 +2,7 @@
 define('PERFORMANCE_TIMER', microtime(TRUE));
 
 defined('APPLICATION_VER')
-	|| define('APPLICATION_VER', '0.5.0-beta');
+	|| define('APPLICATION_VER', '0.5.1-beta');
 
 // Define path to application directory
 defined('APPLICATION_PATH')
